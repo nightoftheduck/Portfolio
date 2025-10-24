2a47c4e6e6ee364948543dadaaa8e39314447015
nightoftheduck <62330688+nightoftheduck@users.noreply.github.com>
2025-10-24 23:11:25 +0800

    Initiating Project

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hi, I'm Yi Jia</h1>
+      <h2>Welcome to my portfolio</h2>
+    </>
+  )
+}
+
+export default App
