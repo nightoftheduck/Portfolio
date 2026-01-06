1da2c36bfe8528d4309698e6cd51f5972bc8aba9
nightoftheduck <62330688+nightoftheduck@users.noreply.github.com>
2026-01-06 22:38:02 +0800

    updated links
    Installed react router dom
    Made a first front page

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
+import { Routes, Route} from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
+import Home from './components/Home.jsx'
 
 function App() {
 
   return (
-    <>
-      <h1>Hi, I'm Yi Jia</h1>
-      <h2>Welcome to my portfolio</h2>
-    </>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/projects" element={<div>Projects Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ Portfolio/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1 className="name">Loke Yi Jia</h1>
+            <h3 className='title'>Software Developer</h3>
+        </div>
+    )
+}
+
+export default Home;
